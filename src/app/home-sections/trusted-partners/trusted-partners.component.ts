18f38e8fcd5b2a3727497a9566e6cb72b3fb4f42
Mani <mani.sundar@paramantra.com>
2025-11-24 03:40:20 +0530

    animation moved oto typescript

--- src/app/home-sections/trusted-partners/trusted-partners.component.ts
+++ src/app/home-sections/trusted-partners/trusted-partners.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HelperService } from 'src/app/services/helper.service';
 
 @Component({
   selector: 'app-trusted-partners',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class TrustedPartnersComponent {
 
+  deviceType = this.helperService.getDeviceFromUserAgent();
+
+  constructor(
+    private helperService: HelperService
+  ) { }
+
+  ngOnInit() {
+    const style = document.createElement('style');
+    style.textContent = `
+    @keyframes moveFlip {
+      0% { transform: translateX(-50px) rotateY(0deg); }
+      45% { transform: translateX(calc(${this.deviceType === 'desktop' ? '50vw' : '100vw'} + 50px)) rotateY(0deg); }
+      50% { transform: translateX(calc(${this.deviceType === 'desktop' ? '50vw' : '100vw'} + 50px)) rotateY(180deg); }
+      95% { transform: translateX(-50px) rotateY(180deg); }
+      100% { transform: translateX(-50px) rotateY(360deg); }
+    }
+
+    .motor-bike {
+      animation: moveFlip ${this.deviceType === 'desktop' ? '10s' : this.deviceType === 'tablet' ? '8s' : '5s'} linear infinite;
+    }
+  `;
+    document.head.appendChild(style);
+  }
 }
